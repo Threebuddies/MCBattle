bf9d40174793f59fa5ac2198a6094a14360c603b
BuildTools <unconfigured@null.spigotmc.org>
2018-05-29 03:40:23 -0400

    Adds possible countdown
    Could work but does not at the time.

---
+++ src/com/gmail/gregrockss/mcbattle/Commands.java
@@ -0,0 +1,33 @@
+package com.gmail.gregrockss.mcbattle;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.WorldBorder;
+
+
+public class Commands extends Main implements Listener, CommandExecutor {
+/*
+	// make a string for the plugin to look for a command
+	public String mcbattle = "mcbattle";
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args ) {		
+		if(cmd.getName().equalsIgnoreCase(mcbattle)) {
+			if(args.length == 0) {	
+				WorldBorder b = Bukkit.getWorld("world").getWorldBorder();
+				b.setCenter(new Location(Bukkit.getWorld("world"), 0, 64, 0));				
+				this.getServer().getScheduler().scheduleAsyncRepeatingTask(this, new Runnable(), 2, 2)
+				public void run(){
+					
+				}				
+			}
+
+		}
+	}
+*/
+}
--- src/com/gmail/gregrockss/mcbattle/Main.java
+++ src/com/gmail/gregrockss/mcbattle/Main.java
@@ -1,13 +1,29 @@
 package com.gmail.gregrockss.mcbattle;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.WorldBorder;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-    @Override
+    private Commands commands = new Commands();
+    
+	@Override
     public void onEnable() {
-       
+		if(cmd.getName().equalsIgnoreCase(mcbattle)) {
+			if(args.length == 0) {	
+				WorldBorder b = Bukkit.getWorld("world").getWorldBorder();
+				b.setCenter(new Location(Bukkit.getWorld("world"), 0, 64, 0));				
+				this.getServer().getScheduler().scheduleAsyncRepeatingTask(this, new Runnable(), 2, 2)
+				public void run(){
+					
+				}				
+			}
+
+		}
+	}
     }
    
     @Override
@@ -19,7 +35,7 @@ public class Main extends JavaPlugin {
             String label,
             String[] args) {
         if (command.getName().equalsIgnoreCase("mcbattle")) {
-            sender.sendMessage("You ran /mycommand!");
+            sender.sendMessage("Command mcbattle has been run!");
             return true;
         }
         return false;
